efe6affceb43b9529c5399bf0b629ec8bc80cd55
Nkuna William <NkunaTebogo09@gmail.com>
2022-02-07 23:18:39 +0200

    Added context structure

--- App.tsx
+++ App.tsx
@@ -1,9 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import TodoScreen from './src/screens/TodoScreen';
+import { Provider } from "./src/context/TodoContext";
 
 export default function App() {
-  return <TodoScreen />;
+  return (
+    <Provider>
+      <TodoScreen />
+    </Provider>
+  )
 }
 
 const styles = StyleSheet.create({});
---
+++ src/context/DataContext.tsx
@@ -0,0 +1,37 @@
+import React, { useReducer } from "react";
+// todo context imports
+import { ToDoState } from "./TodoContext";
+import { ToDoAction } from "./TodoContext";
+import { addToDoItem } from "./TodoContext";
+import { deleteToDoItem } from "./TodoContext";
+import { todoReducerType } from "./TodoContext";
+
+
+export default (reducer: todoReducerType,
+  actions: { addItem: addToDoItem, deleteItem: deleteToDoItem },
+  defaultValue: { id: string, title: string }[]) => {
+  const Context =
+    React.createContext<{ state: ToDoState; dispatch: React.Dispatch<ToDoAction> }>({ state: defaultValue, dispatch: () => { } });
+
+  const Provider = ({ children }: { children: React.ReactNode }) => {
+    const [state, dispatch] = useReducer(reducer, defaultValue);
+
+    const boundActions: any = {};
+    for (let key in actions) {
+      boundActions[key] = (actions as any)[key](dispatch);
+    }
+
+    console.log(boundActions);
+    console.log("***********************");
+    console.log(typeof boundActions);
+
+    return (
+      <Context.Provider value={{ state, ...boundActions }
+      }>
+        {children}
+      </Context.Provider>
+    );
+  };
+
+  return { Provider, Context };
+};
